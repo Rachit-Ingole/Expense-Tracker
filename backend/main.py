115c855701a6ae66b1f78b353f42e6c6f4edce5f
Rachit Ingole <rachitaingole@gmail.com>
2025-02-23 17:52:13 +0530

    ocr changes

--- backend/main.py
+++ backend/main.py
@@ -145,12 +145,15 @@ async def read_user_by_email(email_address: str):
 
 
 # ocr
+from pathlib import Path
+UPLOAD_DIRECTORY = "uploads/"
 @app.post("/api/v1/upload")
 async def upload_image(image: UploadFile = File(...)):
-    image.filename = "/Users/rishitbaitule/PycharmProjects/Expense-Tracker/images/img.png"
+    Path(UPLOAD_DIRECTORY).mkdir(parents=True, exist_ok=True)
+    file_location = Path(UPLOAD_DIRECTORY) / image.filename
     contents = await image.read()
 
-    with open(f"{image.filename}", "wb") as f:
+    with open(f"{file_location}", "wb") as f:
         f.write(contents)
         cost = scan(image.filename)
     os.remove(image.filename)
